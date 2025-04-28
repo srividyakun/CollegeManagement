1d9d3d2ab2c18cdf8fef13c4ee525762c159d950
srividyakundeti <srividya.ece@gmail.com>
2025-04-28 14:54:04 +0200

    First COmmit

---
+++ src/dao/Department.java
@@ -0,0 +1,33 @@
+package dao;
+
+import java.util.List;
+
+public class Department {
+    int departmentId;
+    String departmentName;
+    List<Teacher> teachers;
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(List<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
